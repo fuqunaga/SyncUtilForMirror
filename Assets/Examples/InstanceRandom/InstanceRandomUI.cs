eae8c5281e2cc9efcf507c7cbe92e8776100b989
fuqunaga <fukhid@gmail.com>
2022-07-08 15:11:22 +0900

    update Spawner,ServerOrStandAlone, InstanceRandom Examples

---
+++ Assets/Examples/InstanceRandom/InstanceRandomUI.cs
@@ -0,0 +1,18 @@
+﻿using RosettaUI;
+
+namespace SyncUtil.Example
+{
+    public class InstanceRandomUI : ExampleUIBase
+    {
+        protected override Element CreateElement()
+        {
+            return ExampleTemplate(
+                    @"Deterministic random per instance.
+
+1. Attach the InstanceRandom component to the target GameObject.
+2. Spawn the GameObject because of InstanceRandom inherits NetworkBehaviour.
+3. Use InstanceRandom.CustomRandom from script. It will return same value for all clients."
+            );
+        }
+    }
+}
---
+++ Assets/Examples/Spawner,ServerOrStandAlone/SpawnerAndServerOrStandAloneUI.cs
@@ -0,0 +1,22 @@
+﻿using RosettaUI;
+
+namespace SyncUtil.Example
+{
+    public class SpawnerAndServerOrStandAloneUI : ExampleUIBase
+    {
+        protected override Element CreateElement()
+        {
+            return ExampleTemplate(
+                    @"<b>Spawner</b>
+
+Register prefabs to NetworkManager's spawn prefabs.
+Spawn prefabs when the server is started.
+
+
+<b>ServerOrStandAlone</b>
+
+Deactivate child GameObjects when server or standalone."
+            );
+        }
+    }
+}
